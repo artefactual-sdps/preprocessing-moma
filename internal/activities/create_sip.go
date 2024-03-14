88951a80baa1502aa481102c5372d8b267a70b97
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2024-03-14 13:23:19 -0400

    Remove external python call
    - remove sip.go

--- internal/activities/create_sip.go
+++ internal/activities/create_sip.go
@@ -2,8 +2,6 @@ package activities
 
 import (
 	"context"
-	"errors"
-	"os/exec"
 )
 
 const SipCreationName = "sip-creation"
@@ -24,15 +22,8 @@ type SipCreationResult struct {
 }
 
 func (sc *SipCreationActivity) Execute(ctx context.Context, params *SipCreationParams) (*SipCreationResult, error) {
+	// TODO change this activity into a bag activity
 	res := &SipCreationResult{}
-	e, err := exec.Command("python3", "repackage_sip.py", params.SipPath).CombinedOutput() // #nosec G204
-	if err != nil {
-		return nil, err
-	}
-	res.Out = string(e)
-	if res.Out != params.SipPath+"_bag\n" {
-		return nil, errors.New("Failed to repackage sip correctly: " + res.Out)
-	}
 
 	res.NewSipPath = params.SipPath + "_bag"
 	return res, nil
