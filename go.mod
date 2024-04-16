905b99e9bc38f512926e4237ade509cf1ff13816
David Juhasz <djjuhasz@gmail.com>
2024-04-16 16:59:11 -0700

    Add a preprocessing workflow unit test

--- go.mod
+++ go.mod
@@ -3,16 +3,20 @@ module github.com/artefactual-sdps/preprocessing-moma
 go 1.22.2
 
 require (
+	github.com/artefactual-sdps/remove-files-activity v0.0.0-20240301164716-575fb8b7b834
 	github.com/artefactual-sdps/temporal-activities v0.0.0-20240418211119-e97b26fed41b
 	github.com/go-logr/logr v1.4.1
+	github.com/otiai10/copy v1.14.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.9.0
 	go.artefactual.dev/tools v0.8.0
 	go.temporal.io/sdk v1.26.1
 	gotest.tools/v3 v3.5.1
 )
 
 require (
+	github.com/Diogenesoftoronto/go-gitignore v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -36,13 +40,13 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.temporal.io/api v1.32.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
---
+++ internal/workflow/preprocessing_test.go
@@ -0,0 +1,105 @@
+package workflow_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/artefactual-sdps/preprocessing-moma/internal/config"
+	"github.com/artefactual-sdps/preprocessing-moma/internal/workflow"
+	remove "github.com/artefactual-sdps/remove-files-activity"
+
+	"github.com/artefactual-sdps/temporal-activities/removefiles"
+	cp "github.com/otiai10/copy"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	temporalsdk_activity "go.temporal.io/sdk/activity"
+	"go.temporal.io/sdk/testsuite"
+	temporalsdk_worker "go.temporal.io/sdk/worker"
+)
+
+type PreprocessingTestSuite struct {
+	suite.Suite
+	testsuite.WorkflowTestSuite
+
+	env *testsuite.TestWorkflowEnvironment
+
+	// Each test creates its own temporary transfer directory.
+	testDir string
+
+	// Each test registers the workflow with a different name to avoid
+	// duplicates.
+	workflow *workflow.PreprocessingWorkflow
+}
+
+func (s *PreprocessingTestSuite) copyTestTransfer(src, relPath string) {
+	err := cp.Copy(src, filepath.Join(s.testDir, relPath))
+	if err != nil {
+		s.T().Fatalf("copyTestTransfer: %v", err)
+	}
+}
+
+func (s *PreprocessingTestSuite) SetupTest(
+	cfg config.Configuration,
+	transferPath, relPath string,
+) {
+	s.env = s.NewTestWorkflowEnvironment()
+	s.env.SetWorkerOptions(temporalsdk_worker.Options{EnableSessionWorker: true})
+	s.testDir = s.T().TempDir()
+
+	// Register activities.
+	s.env.RegisterActivityWithOptions(
+		removefiles.NewActivity(cfg.RemoveFiles).Execute,
+		temporalsdk_activity.RegisterOptions{Name: remove.RemoveFilesName},
+	)
+
+	s.workflow = workflow.NewPreprocessingWorkflow(s.testDir)
+
+	// Copy test files to testDir.
+	s.copyTestTransfer(transferPath, relPath)
+}
+
+func (s *PreprocessingTestSuite) AfterTest(suiteName, testName string) {
+	s.env.AssertExpectations(s.T())
+}
+
+func TestPreprocessingWorkflow(t *testing.T) {
+	suite.Run(t, new(PreprocessingTestSuite))
+}
+
+func (s *PreprocessingTestSuite) TestExecute() {
+	relPath := "transfer"
+	cfg := config.Configuration{
+		RemoveFiles: removefiles.Config{RemoveNames: ".DS_Store"},
+	}
+
+	s.SetupTest(
+		cfg,
+		filepath.Join("testdata", "transfer_with_ds_store"),
+		relPath,
+	)
+
+	// Mock activities.
+	sessionCtx := mock.AnythingOfType("*context.timerCtx")
+	s.env.OnActivity(
+		removefiles.ActivityName,
+		sessionCtx,
+		&removefiles.ActivityParams{Path: filepath.Join(s.testDir, relPath)},
+	).Return(
+		&removefiles.ActivityResult{Count: 1}, nil,
+	)
+
+	s.env.ExecuteWorkflow(
+		s.workflow.Execute,
+		&workflow.PreprocessingWorkflowParams{RelativePath: relPath},
+	)
+
+	s.True(s.env.IsWorkflowCompleted())
+
+	var result workflow.PreprocessingWorkflowResult
+	err := s.env.GetWorkflowResult(&result)
+	s.NoError(err)
+	s.Equal(
+		&result,
+		&workflow.PreprocessingWorkflowResult{RelativePath: relPath},
+	)
+}
