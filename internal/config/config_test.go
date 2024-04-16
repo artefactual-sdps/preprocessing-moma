552e1aa523d452ffab9a2deb8e141b4a398d3fb4
David Juhasz <djjuhasz@gmail.com>
2024-04-16 14:20:00 -0700

    Replace remove files activity
    Fixes #1
    
    Replace "github.com/artefactual-sdps/remove-files-activity" with
    "github.com/artefactual-sdps/temporal-activities/removefiles".
    
    Also:
    - Remove example ".remove" and ".succumb" files
    - Remove creation of ".remove" file from kube dev overlay

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -11,8 +11,17 @@ import (
 
 const testConfig = `# Config
 debug = true
+verbosity = 2
+sharedPath = "/tmp"
 [temporal]
 address = "host:port"
+namespace = "default"
+taskQueue = "preprocessing"
+workflowName = "preprocessing"
+[worker]
+maxConcurrentSessions = 1
+[removeFiles]
+removeNames = ".DS_Store, Thumbs.db"
 `
 
 func TestConfig(t *testing.T) {
@@ -31,5 +40,17 @@ func TestConfig(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, found, true)
 	assert.Equal(t, configFileUsed, configFile)
+
+	assert.Equal(t, c.Debug, true)
+	assert.Equal(t, c.Verbosity, 2)
+	assert.Equal(t, c.SharedPath, "/tmp")
+
 	assert.Equal(t, c.Temporal.Address, "host:port")
+	assert.Equal(t, c.Temporal.Namespace, "default")
+	assert.Equal(t, c.Temporal.TaskQueue, "preprocessing")
+	assert.Equal(t, c.Temporal.WorkflowName, "preprocessing")
+
+	assert.Equal(t, c.Worker.MaxConcurrentSessions, 1)
+
+	assert.Equal(t, c.RemoveFiles.RemoveNames, ".DS_Store, Thumbs.db")
 }
